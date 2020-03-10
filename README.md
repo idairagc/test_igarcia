b10e4e6f4f447c46d68cf879cc568fd27b76a957
idairagc <61988818+idairagc@users.noreply.github.com>
2020-03-10 19:29:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# test_igarcia
+ test idaira garcia
---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')->accessToken; 
+            $success['name'] =  $user->name;
+            
+            $response = [
+	            'success' => true,
+	            'data'    => $success,
+	            'message' => 'User login successfully.',
+	        ];
+   
+            return response()->json($response, 200);
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401);
+        } 
+    }
+}
